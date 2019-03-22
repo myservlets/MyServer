bb947d809f2d6885780dd67e5eb6c78312c5d972
whu_s <whu_swh@whu.edu.cn>
2019-03-22 13:49:45 +0800

    hu1

---
+++ src/entity/Goods.java
@@ -0,0 +1,10 @@
+package entity;
+
+public class Goods {
+    private String goodsId;
+    private String goodsName;
+    private double price;
+    private String userId;
+    private int quantity;
+
+}
